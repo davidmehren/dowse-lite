254ede4500ab2ffa7b281b70c5d2e21aa88ff526
David Mehren <david.mehren@udo.edu>
2017-11-11 17:59:39 +0100

    Do all the piping internally, using subprocess.

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This needs `pyshark`.
 ## Usage
 - Set your interface and your local IP in `main.py` (this should be confgurable).
 - Start the script.
-- Use something like `cat /tmp/dowsefifo |gource --realtime --log-format custom -` to start the visualization.
+- Gource should automatically open.
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import socket
 from functools import lru_cache
 import pyshark
-import os, tempfile
+from subprocess import Popen, PIPE
+import sys
 
 
-@lru_cache(maxsize=1024)
 def get_domain(ip):
     try:
         return socket.gethostbyaddr(ip)[0]
@@ -12,25 +12,34 @@ def get_domain(ip):
         return ip
 
 
+@lru_cache(maxsize=1024)
 def get_domain_str(ip):
-    domain = get_domain(ip)
-    return domain.split(".")[-1]+"/"+domain
+    try:
+        domain = socket.gethostbyaddr(ip)[0]
+        return domain.split(".")[-1] + "/" + domain
+    except socket.herror:
+        return "IPs/" + ip
 
 
-tmpdir = tempfile.mkdtemp()
-pipefile = "/tmp/dowsefifo"
-print("Using FIFO", pipefile)
-try:
-    os.mkfifo(pipefile)
-except OSError as e:
-    print("Failed to create FIFO: %s" % e)
-with open(pipefile, 'w') as fifo:
+def main_app():
+    print("Initializing Gource...")
+    gource = Popen(["gource", "-realtime", "-log-format", "custom" ,"-"], stdin=PIPE, universal_newlines=True)
     print("Initializing Capture...")
-    capture = pyshark.LiveCapture(interface='bnep0', bpf_filter="tcp", display_filter="ip.dst != 192.168.44.129")
-    capture.set_debug()
+    capture = pyshark.LiveCapture(interface='wlp3s0', bpf_filter="tcp", display_filter="ip.dst != 192.168.178.73")
     for packet in capture.sniff_continuously():
         try:
-            fifo.write(packet.sniff_timestamp.split(".")[0] + "|" + packet.ip.src + "|M|" + get_domain_str(packet.ip.dst)+"\n")
-            fifo.flush()
+            gource.stdin.write(packet.sniff_timestamp.split(".")[0] + "|" + packet.ip.src + "|M|" + get_domain_str(
+                packet.ip.dst) + "\n")
+            gource.stdin.flush()
         except AttributeError:
-            pass #print(packet)
+            pass
+        except KeyboardInterrupt:
+            capture.close()
+            gource.terminate()
+            sys.exit(0)
+
+
+try:
+    main_app()
+except KeyboardInterrupt:
+    print("EXIT!")
