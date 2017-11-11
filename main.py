d6f888c0376d71194f75aa9194a671cd9d5f5925
David Mehren <david.mehren@udo.edu>
2017-11-11 19:41:16 +0100

    Add DNS mode, argparsing and autofiltering of incoming packets

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import re
 import socket
 from functools import lru_cache
+
+import os
 import pyshark
 from subprocess import Popen, PIPE
 import sys
+import argparse
 
 
 def get_domain(ip):
@@ -21,15 +25,41 @@ def get_domain_str(ip):
         return "IPs/" + ip
 
 
-def main_app():
+def guess_ip(interface):
+    print("Trying to guess IP of interface " + interface + " ...")
+    ipv4 = re.search(re.compile(r'(?<=inet )(.*)(?=\/)', re.M), os.popen('ip addr show ' + interface).read()).groups()[
+        0]
+    print("My IP seems to be:", ipv4)
+    return ipv4
+
+
+def create_capture(interface, myip, mode):
+    if mode == "dns":
+        return pyshark.LiveCapture(interface=interface, display_filter="dns and ip.dst != %s" % myip)
+    elif mode == "tcp":
+        return pyshark.LiveCapture(interface=interface, bpf_filter="tcp", display_filter="ip.dst != %s" % myip)
+
+
+def generate_packet_str(packet, mode):
+    if mode == "dns":
+        return packet.sniff_timestamp.split(".")[0] + "|" + packet.ip.src + "|M|" + packet.dns.qry_name.split(".")[
+            -1] + "/" + packet.dns.qry_name + "\n"
+    elif mode == "tcp":
+        return packet.sniff_timestamp.split(".")[0] + "|" + packet.ip.src + "|M|" + get_domain_str(
+            packet.ip.dst) + "\n"
+
+
+def main_app(args):
+    print("Running in mode: " + args.mode)
+    if args.myip is None:
+        args.myip = guess_ip(args.interface)
     print("Initializing Gource...")
-    gource = Popen(["gource", "-realtime", "-log-format", "custom" ,"-"], stdin=PIPE, universal_newlines=True)
+    gource = Popen(["gource", "-realtime", "-log-format", "custom", "-"], stdin=PIPE, universal_newlines=True)
     print("Initializing Capture...")
-    capture = pyshark.LiveCapture(interface='wlp3s0', bpf_filter="tcp", display_filter="ip.dst != 192.168.178.73")
+    capture = create_capture(args.interface, args.myip, args.mode)
     for packet in capture.sniff_continuously():
         try:
-            gource.stdin.write(packet.sniff_timestamp.split(".")[0] + "|" + packet.ip.src + "|M|" + get_domain_str(
-                packet.ip.dst) + "\n")
+            gource.stdin.write(generate_packet_str(packet, args.mode))
             gource.stdin.flush()
         except AttributeError:
             pass
@@ -40,6 +70,12 @@ def main_app():
 
 
 try:
-    main_app()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("interface", help="Interface to detect traffic on", type=str)
+    parser.add_argument("--mode", help="Use RDNS of all TCP traffic (tcp, default) or analyse DNS querys (dns)",
+                        default="tcp", choices=["tcp", "dns"])
+    parser.add_argument("--myip", help="Set IP of this device if the autodetection leads to wrong results", type=str)
+    args = parser.parse_args()
+    main_app(args)
 except KeyboardInterrupt:
     print("EXIT!")
